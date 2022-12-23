abff320e32b356eeb14f06cc980daf8a2203e020
Matias Cabot <matiascabot97@gmail.com>
2022-12-23 11:11:47 -0300

    Implementacion reCaptcha

--- js/contact.form.js
+++ js/contact.form.js
@@ -2,25 +2,46 @@
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
 
-   btn.value = 'Sending...';
+        btn.value = 'Sending...';
 
-   const serviceID = 'service_83c8xaw';
-   const templateID = 'template_2m1kx7n';
+        const serviceID = 'service_83c8xaw';
+        const templateID = 'template_2m1kx7n';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      Swal.fire(
-        'Mail Sent',
-        '',
-        'success'
-      )
-      form.reset();
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
+        if (grecaptcha.getResponse().length === 0){
+            Swal.fire({
+                icon: 'error',
+                title: 'Captcha incomplete',
+                text: '',
+                confirmButtonColor: '#41b6e6',
+            })
+        }
+        else {
+            emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Email sent!',
+                    text: '',
+                    confirmButtonColor: '#41b6e6',
+                })
+                grecaptcha.reset();
+                form.reset();
+                }, (err) => {
+                btn.value = 'Send Email';
+                Swal.fire({
+                    icon: 'error',
+                    title: 'error',
+                    text: 'Try again later',
+                    confirmButtonColor: '#41b6e6',
+                })
+                grecaptcha.reset();
+                form.reset();
+            }); 
+        }
+
+        
 });
